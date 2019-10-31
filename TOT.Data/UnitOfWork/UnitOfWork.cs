cf550247ebf55ec09e916b71656d3c83443865cb
Dewold <56039693+Dewold@users.noreply.github.com>
2019-10-31 10:21:52 +0200

    Добавлена реализация IDisposable в класс UnitOfWork

--- TOT.Data/UnitOfWork/UnitOfWork.cs
+++ TOT.Data/UnitOfWork/UnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using TOT.Data.Repositories;
 using TOT.Entities;
 using TOT.Interfaces;
@@ -6,10 +7,12 @@ using TOT.Interfaces.Repositories;
 
 namespace TOT.Data.UnitOfWork
 {
-    public class UnitOfWork : IUnitOfWork
+    public class UnitOfWork : IUnitOfWork, IDisposable
     {
         private ApplicationDbContext context;
         private UserInformationRepository userInfoRepository;
+
+        private bool disposed = false;
 
         public UnitOfWork(DbContextOptions<ApplicationDbContext> options)
         {
@@ -29,6 +32,24 @@ namespace TOT.Data.UnitOfWork
         public void Save()
         {
             context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
