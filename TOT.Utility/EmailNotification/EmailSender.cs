8a76e7807117f17c53130e43df7dcff512616a7e
Dewold <56039693+Dewold@users.noreply.github.com>
2019-11-02 16:25:27 +0200

    Добавлена функция отправки Email пользователям
    1. Для подтверждения электронной почты
    2. Для смены пароля в "Forget password?"

--- TOT.Utility/EmailNotification/EmailSender.cs
+++ TOT.Utility/EmailNotification/EmailSender.cs
@@ -1,16 +1,51 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
 using System.Threading.Tasks;
+using TOT.Interfaces;
 
 namespace TOT.Utility.EmailNotification
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        private readonly IEmailSendConfiguration emailConfiguration;
+
+        public EmailSender(IEmailSendConfiguration emailconfig)
         {
-            throw new NotImplementedException();
+            emailConfiguration = emailconfig;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MimeMessage message = new MimeMessage();
+
+            try
+            {
+                message.From.Add(new MailboxAddress("(no-reply) Time off tracker", emailConfiguration.SmtpUsername));
+                message.To.Add(new MailboxAddress(email, email));
+                message.Subject = subject;
+
+                message.Body = new TextPart(TextFormat.Html)
+                {
+                    Text = htmlMessage
+                };
+
+                using (var emailClient = new SmtpClient())
+                {
+                    //emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+                    await emailClient.ConnectAsync(emailConfiguration.SmtpServer, emailConfiguration.SmtpPort, false);                    
+                    await emailClient.AuthenticateAsync(emailConfiguration.SmtpUsername, emailConfiguration.SmtpPassword);
+
+                    await emailClient.SendAsync(message);
+                    await emailClient.DisconnectAsync(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException("An error occurred while sending the Email" + ex.Message);
+            }
         }
     }
 }
