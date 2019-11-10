ebe955fa37d52506f171f1812d6519ed3e0e4ac5
Timur Mirzaiev <timur.mirzaiev@gmail.com>
2019-11-10 13:12:40 +0200

    created base template

---
+++ TOT.Tests/Controller/VacationControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TOT.Entities;
+using TOT.Interfaces.Repositories;
+using TOT.Web.Controllers;
+using Xunit;
+
+namespace TOT.Tests.Controller {
+    /* public class VacationControllerTests {
+         [Fact]
+          public void VacationRequest_Returns()
+          {
+              // Arrange
+              var mock = new Mock<IRepository<VacationRequest>>();
+              mock.Setup(repo => repo.GetAll()).Returns(GetVacationRequests());
+              var controller = new VacationController(mock.Object);
+
+              // Act
+              var result = controller.Index();
+
+              // Assert
+              var viewResult = Assert.IsType<ViewResult>(result);
+              var model = Assert.IsAssignableFrom<IEnumerable<VacationRequest>>(viewResult.Model);
+              Assert.Equal(GetVacationRequests().Count, model.Count());
+
+          }
+          private ICollection<VacationRequest> GetVacationRequests()
+          {
+              ICollection<VacationRequest> vacations = new List<VacationRequest>();
+              vacations.Add(new VacationRequest()
+              {
+                  //StartDate = DateTime.
+              });
+          }
+      }*/
+}
