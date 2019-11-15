c17282b264671b4cbef27a3e5c5db5fa65bb049a
Timur Mirzaiev <timur.mirzaiev@gmail.com>
2019-11-15 21:38:01 +0200

    Revert "added EmailSender for sending messages to employees and managers"
    This reverts commit 4332aa57ef6f5da17b2012c8c872860e65edecb6.

--- TOT.Data/UnitOfWork/UnitOfWork.cs
+++ TOT.Data/UnitOfWork/UnitOfWork.cs
@@ -94,7 +94,6 @@ namespace TOT.Data.UnitOfWork
 
         // This code added to correctly implement the disposable pattern.
         public void Dispose()
-        
         {
             // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
