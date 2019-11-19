d0230299bb89eb25f0cac04488bef0aff17fa5ed
Timur Mirzaiev <timur.mirzaiev@gmail.com>
2019-11-19 14:28:49 +0200

    fix style

--- TOT.Business/Services/VacationEmailSender.cs
+++ TOT.Business/Services/VacationEmailSender.cs
@@ -32,7 +32,7 @@ namespace TOT.Business.Services {
             htmlContent += $"<a href='{host}/Manager/Index'>Follow the link</a>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
 
-            var response = await client.SendEmailAsync(msg);
+            //var response = await client.SendEmailAsync(msg);
         }
         public async void ExecuteToEmployeeAccept(EmailModel model)
         {
@@ -48,7 +48,7 @@ namespace TOT.Business.Services {
             htmlContent += $"<a href='{host}/Manager/Index'>Follow the link</a>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
 
-            var response = await client.SendEmailAsync(msg);
+            //var response = await client.SendEmailAsync(msg);
         }
         public async void ExecuteToEmployeeDecline(EmailModel model)
         {
@@ -64,7 +64,7 @@ namespace TOT.Business.Services {
             htmlContent += $"<a href='{host}/Manager/Index'>Follow the link</a>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
 
-            var response = await client.SendEmailAsync(msg);
+            //var response = await client.SendEmailAsync(msg);
         }
     }
 }
