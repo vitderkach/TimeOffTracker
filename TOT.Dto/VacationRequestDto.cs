c17f2aca505ce203541f4fc3848983bcd1d031a8
AleksieienkoValeriia <vo.aleksieienko@gmail.com>
2020-04-22 20:57:14 +0300

    Updated pages` localization. Supplemented resource files.

--- TOT.Dto/VacationRequestDto.cs
+++ TOT.Dto/VacationRequestDto.cs
@@ -11,16 +11,24 @@ namespace TOT.Dto
     {
         public ApplicationDto ApplicationDto { get; set; }
 
+        [Display(Name ="From")]
         public DateTime StartDate { get; set; }
+
+        [Display(Name ="To")]
         public DateTime EndDate { get; set; }
+
         public bool? Approval { get; set; }
 
         [Display(Name = "Status")]
         public int Stage { get; set; }
+
         [Display(Name = "Vacation type")]
         public TimeOffType VacationType { get; set; }
+
         public ICollection<ManagerResponseListDto> AllManagerResponses { get; set; }
+
         public bool? SelfCancelled { get; set; }
+
         public UserInformationDto User { get; set; }
     }
 }
